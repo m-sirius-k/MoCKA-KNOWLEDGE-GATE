5711639986fa05060790cabcd566f487db32fc24
nsjpkimura-del <nsjpkimura@gmail.com>
2025-11-13 11:34:30 +0900

    Delete pages/api directory

--- pages/api/trigger.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    res.status(200).json({ status: "success", message: "API is working!" });
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
-  }
-}
